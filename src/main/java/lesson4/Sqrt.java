fc3a975b0e0c38ca133def59c5fdceaf577d3ad3
alissakot <chirukhina.nad@yandex.ru>
2021-11-13 00:18:50 +0300

    add fourth lesson

---
+++ src/main/java/lesson4/Sqrt.java
@@ -0,0 +1,33 @@
+package lesson4;
+
+import java.util.Scanner;
+
+public class Sqrt {
+    public static int value;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        String newValue = scanner.nextLine();
+        value = Integer.parseInt(newValue);
+        result();
+    }
+
+    public static void result() {
+        int n = 2;
+        if (value == 1) {
+            System.out.println("корень числа " + 1);
+            return;
+        }
+        while (value >= n * n) {
+            if (n * n == value) {
+                System.out.println("корень числа " + value + " " + n);
+                break;
+            }
+            n++;
+        }
+        if (n * n > value) {
+            System.out.println("нет действительных корней");
+        }
+    }
+}
