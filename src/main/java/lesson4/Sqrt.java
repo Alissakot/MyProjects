388e924320f53934e2e9805e3801f15a4e644055
alissakot <chirukhina.nad@yandex.ru>
2021-11-16 21:24:53 +0300

    lesson fourth is fixed

--- src/main/java/lesson4/Sqrt.java
+++ src/main/java/lesson4/Sqrt.java
@@ -3,35 +3,35 @@ package lesson4;
 import java.util.Scanner;
 
 public class Sqrt {
-    public static int value;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число: ");
         String newValue = scanner.nextLine();
-        value = Integer.parseInt(newValue);
-        result();
+        int value = Integer.parseInt(newValue);
+        result(value);
     }
 
-    public static void result() {
+    public static void result(int value) {
         int n = 2;
-        if (true) {
-            if (value == 1) {
-                System.out.println("корень числа " + 1);
-                return;
+        if (value == 1) {
+            System.out.println("корень числа равен 1");
+            return;
+        }else if(value==0){
+            System.out.println("корень числа равен нулю");
+            return;
+        }
+
+        while (value >= n * n) {
+            if (n * n == value) {
+                System.out.println("корень числа " + value + " равен " + n);
+                break;
             }
-            while (value >= n * n) {
-                if (n * n == value) {
-                    System.out.println("корень числа " + value + " " + n);
-                    break;
-                }
-                n++;
-            }
-            if (n * n > value) {
-                System.out.println("нет действительных корней");
-            }
-        } else {
+            n++;
+        }
+        if (n * n > value) {
             System.out.println("нет действительных корней");
         }
+
     }
 }
