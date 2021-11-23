fc130f3e7a9f2e0194afd28ae1fb9e32191a050c
alissakot <chirukhina.nad@yandex.ru>
2021-11-23 17:01:20 +0300

    lesson 7

---
+++ src/main/java/lesson7/Cat.java
@@ -0,0 +1,31 @@
+package lesson7;
+
+public class Cat {
+    String name;
+    int age;
+    int run;
+    int swim;
+
+    public Cat(String name, int age, int run, int swim) {
+        this.name = name;
+        this.age = age;
+        this.run = run;
+        this.swim = swim;
+    }
+    public void setRunSwim(int run, int swim) {
+        if(run>200){
+            run = 200;
+        }
+
+        this.run = run;
+        this.swim = 0;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public String getSwim (){
+        return ("не стал плавать");
+    }
+}
