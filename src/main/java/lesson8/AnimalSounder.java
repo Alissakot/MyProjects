20ef7da2cb6baf2c6453d2d41e7c35e256f51e56
alissakot <chirukhina.nad@yandex.ru>
2021-12-07 22:01:05 +0300

    lesson 8 fixed

--- src/main/java/lesson8/AnimalSounder.java
+++ src/main/java/lesson8/AnimalSounder.java
@@ -5,8 +5,8 @@ public class AnimalSounder {
     public static void main(String[] args) {
         Animal Gavrusha = new Cats("Гаврюша", 3, "мяу");
         Animal Bobik = new Dogs("Бобик", 1, "гав");
-        Animal Kesha = new Cats("Кеша", 5, "чирик");
-        Animal Murka = new Cats("Мурка", 2, "муу");
+        Animal Kesha = new Parrot("Кеша", 5, "чирик");
+        Animal Murka = new Cow("Мурка", 2, "муу");
 
         Animal[] AnimalSounder= new Animal[] {Gavrusha, Bobik, Kesha, Murka};
         for (Animal anim : AnimalSounder) {
