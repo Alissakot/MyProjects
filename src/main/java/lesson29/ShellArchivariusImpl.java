e2fa7812eb9bd2a43c8a0de2f999356ed70a1ca2
alissakot <chirukhina.nad@yandex.ru>
2022-03-06 21:49:57 +0300

    lesson 32

--- src/main/java/lesson29/ShellArchivariusImpl.java
+++ src/main/java/lesson29/ShellArchivariusImpl.java
@@ -1,73 +1,73 @@
 package lesson29;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
-
-@Slf4j
-@ShellComponent
-@RequiredArgsConstructor
-public class ShellArchivariusImpl {
-
-    private final LocalisationService localizationService;
-
-    @ShellMethod(value = "packaging", key = {"pack", "pa", "1"})
-    public String pack(String filePath) {
-        String filePathOutput = filePath + ".zip";
-        String fileName = "";
-        int index1 = filePath.lastIndexOf("\\");
-        for (int i = index1 + 1; i < filePath.length(); i++) {
-            fileName += filePath.charAt(i);
-        }
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(filePathOutput));
-             FileInputStream fis = new FileInputStream(filePath)) {
-            ZipEntry entry1 = new ZipEntry(fileName);
-            zout.putNextEntry(entry1);
-            byte[] buffer = new byte[fis.available()];
-            fis.read(buffer);
-            zout.write(buffer);
-            zout.closeEntry();
-        } catch (Exception ex) {
-
-            System.out.println(ex.getMessage());
-        }
-        return localizationService.localize("pack", fileName);
-    }
-
-    @ShellMethod(value = "Unpack", key = {"unpack", "un", "2"})
-    public String unpack(String filePath, String fileOutputPath) {
-
-        int index = fileOutputPath.lastIndexOf("\\");
-        if (index != fileOutputPath.length() - 1) {
-            fileOutputPath += "\\";
-        }
-        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(filePath))) {
-            ZipEntry entry;
-            String name;
-            long size;
-            while ((entry = zin.getNextEntry()) != null) {
-                name = entry.getName();
-                size = entry.getSize();
-                System.out.printf("File name: %s \t File size: %d \n", name, size);
-                FileOutputStream fout = new FileOutputStream(fileOutputPath + name);
-                while (zin.available() > 0) {
-                    fout.write(zin.read());
-                }
-                fout.flush();
-                zin.closeEntry();
-                fout.close();
-            }
-        } catch (Exception ex) {
-
-            System.out.println(ex.getMessage());
-        }
-        return localizationService.localize("unpack", fileOutputPath);
-    }
-}
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.shell.standard.ShellComponent;
+//import org.springframework.shell.standard.ShellMethod;
+//
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
+//import java.util.zip.ZipEntry;
+//import java.util.zip.ZipInputStream;
+//import java.util.zip.ZipOutputStream;
+//
+//@Slf4j
+//@ShellComponent
+//@RequiredArgsConstructor
+//public class ShellArchivariusImpl {
+//
+//    private final LocalisationService localizationService;
+//
+//    @ShellMethod(value = "packaging", key = {"pack", "pa", "1"})
+//    public String pack(String filePath) {
+//        String filePathOutput = filePath + ".zip";
+//        String fileName = "";
+//        int index1 = filePath.lastIndexOf("\\");
+//        for (int i = index1 + 1; i < filePath.length(); i++) {
+//            fileName += filePath.charAt(i);
+//        }
+//        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(filePathOutput));
+//             FileInputStream fis = new FileInputStream(filePath)) {
+//            ZipEntry entry1 = new ZipEntry(fileName);
+//            zout.putNextEntry(entry1);
+//            byte[] buffer = new byte[fis.available()];
+//            fis.read(buffer);
+//            zout.write(buffer);
+//            zout.closeEntry();
+//        } catch (Exception ex) {
+//
+//            System.out.println(ex.getMessage());
+//        }
+//        return localizationService.localize("pack", fileName);
+//    }
+//
+//    @ShellMethod(value = "Unpack", key = {"unpack", "un", "2"})
+//    public String unpack(String filePath, String fileOutputPath) {
+//
+//        int index = fileOutputPath.lastIndexOf("\\");
+//        if (index != fileOutputPath.length() - 1) {
+//            fileOutputPath += "\\";
+//        }
+//        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(filePath))) {
+//            ZipEntry entry;
+//            String name;
+//            long size;
+//            while ((entry = zin.getNextEntry()) != null) {
+//                name = entry.getName();
+//                size = entry.getSize();
+//                System.out.printf("File name: %s \t File size: %d \n", name, size);
+//                FileOutputStream fout = new FileOutputStream(fileOutputPath + name);
+//                while (zin.available() > 0) {
+//                    fout.write(zin.read());
+//                }
+//                fout.flush();
+//                zin.closeEntry();
+//                fout.close();
+//            }
+//        } catch (Exception ex) {
+//
+//            System.out.println(ex.getMessage());
+//        }
+//        return localizationService.localize("unpack", fileOutputPath);
+//    }
+//}
--- src/main/java/lesson29/config/AppConfig.java
+++ src/main/java/lesson29/config/AppConfig.java
@@ -1,20 +1,20 @@
 package lesson29.config;
-
-import lesson29.LocalisationService;
-import lombok.SneakyThrows;
-import org.jline.utils.AttributedString;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.shell.jline.PromptProvider;
-
-@Configuration
-@EnableConfigurationProperties
-public class AppConfig {
-
-    @Bean
-    @SneakyThrows
-    public PromptProvider promptProvider(LocalisationService localisationService) {
-        return () -> new AttributedString(localisationService.localize("intro"));
-    }
-}
+//
+//import lesson29.LocalisationService;
+//import lombok.SneakyThrows;
+//import org.jline.utils.AttributedString;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.shell.jline.PromptProvider;
+//
+//@Configuration
+//@EnableConfigurationProperties
+//public class AppConfig {
+//
+//    @Bean
+//    @SneakyThrows
+//    public PromptProvider promptProvider(LocalisationService localisationService) {
+//        return () -> new AttributedString(localisationService.localize("intro"));
+//    }
+//}
